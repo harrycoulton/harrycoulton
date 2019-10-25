715dfd20349c7104d6a138cc364083b0596a1429
harrycoulton <hfcoulton93@gmail.com>
2019-10-25 18:22:02 +0100

    changed the layout somewhat, with a smooth-scrolling, single page approach. Started work on animating various parts of the page, however after this commit I will be ripping out a lot of what exists to bring in more interesting and functional javascript rather than CSS. Commit is mainly so that I can work on a different machine with the current unfinished codebase.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-document.querySelector('a').addEventListener('click', function() {
-})
+anime({
+    targets: '.what',
+    translateX: function(el) {
+        return el.getAttribute('data-x');
+    },
+    translateY: function(el, i) {
+        return 50 + (-50 * i);
+    },
+    scale: function(el, i, l) {
+        return (l - i) + .25;
+    },
+    rotate: function() { return anime.random(-360, 360); },
+    borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
+    duration: function() { return anime.random(1200, 1800); },
+    delay: function() { return anime.random(0, 400); },
+    direction: 'alternate',
+    loop: true
+});
